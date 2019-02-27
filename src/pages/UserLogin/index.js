399d19caa06f983be85d2c5cfa9dbffce6d9d99d
John周公来 <john.zhou@abc360.com>
2019-02-27 13:59:25 +0800

    type: feat 优化

--- src/pages/UserLogin/index.js
+++ src/pages/UserLogin/index.js
@@ -1,3 +1,12 @@
-import UserLogin from './UserLogin';
+import React from 'react';
+import Loadable from 'react-loadable';
+import { Loading } from '@alifd/next';
 
-export default UserLogin;
+/**
+ * @EXAMPLE
+ * 骨架屏 loader
+*/
+export default Loadable({
+  loader: () => import(/* webpackChunkName: "UserLogin" */ './UserLogin'),
+  loading: () => <Loading className="fit-content" />,
+});
